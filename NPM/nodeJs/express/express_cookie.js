2f737b3d21678f68db712d8c1b9b1b024e0bd6fd
wangxin <wangxin@jtoushou.com>
2017-08-25 18:02:28 +0800

    2017.08.25

---
+++ NPM/nodeJs/express/express_cookie.js
@@ -0,0 +1,15 @@
+/**
+ * Created by David on 2017/8/17.
+ */
+// express_cookie.js 文件
+var express      = require('express')
+var cookieParser = require('cookie-parser')
+
+var app = express()
+app.use(cookieParser())
+
+app.get('/', function(req, res) {
+    console.log("Cookies: ", req.cookies)
+})
+
+app.listen(8081)
---
+++ NPM/nodeJs/multiprocess/support.js
@@ -0,0 +1,4 @@
+/**
+ * Created by David on 2017/8/17.
+ */
+console.log("进程 " + process.argv[2] + " 执行。" );
