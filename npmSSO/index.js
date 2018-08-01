2f8a44d16021249d39d85ba337d6255133179fa5
wangxin <wangxin@jtoushou.com>
2018-08-01 15:45:25 +0800

    111

--- npmSSO/index.js
+++
@@ -1,9 +0,0 @@
-exports.ssoPlugins={
-    ssoHost:'https://alpha-passport.jintoushou.com',
-    apiHost:'http://127.0.0.1:8101',
-    localLogoutUrl:'/logout.html',
-    localService:'http://b.jts.com:88',
-    filterReg:/^\/user\//,
-    logoutReg:/^\/logout.html\//,
-
-};
